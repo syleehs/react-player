f6e306c166deaa3f57ab076dd96655548f65e2a6
Yannick Lee <lee.yannick@gmail.com>
2021-01-08 00:35:59 -0500

    Added some base functionalities to the  webpage, it retrieves a few songs as well as displays some information + plays & pauses the songs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React, { useState } from "react";
+//Adding Components
+import Player from "./components/Player";
+import Song from "./components/Song";
+//Adding Style
+import "./styles/app.scss";
+//Import util
+import data from "./util";
+
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-      <h1>React Music Player</h1>
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
---
+++ src/components/Player.js
@@ -0,0 +1,47 @@
+import React, { useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleRight,
+  faAngleLeft,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+  //Ref
+  const audioRef = useRef(null);
+  //Event Handlers
+  const playSongHandler = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>Start Time</p>
+        <input type="range" />
+        <p>End Time</p>
+      </div>
+      <div className="play-control">
+        <FontAwesomeIcon className="Skip-back" size="2x" icon={faAngleLeft} />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className="Play"
+          size="2x"
+          icon={faPlay}
+        />
+        <FontAwesomeIcon
+          className="Skip-forward"
+          size="2x"
+          icon={faAngleRight}
+        />
+      </div>
+      <audio ref={audioRef} src={currentSong.audio}></audio>
+    </div>
+  );
+};
+
+export default Player;
